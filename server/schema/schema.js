fdc461615b16feb80753308f6894ba5fa84b4802
Andrew Zabelin <a.zabelin1@yandex.ru>
2020-04-27 17:24:39 +0300

    add resolve method

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,11 +1,18 @@
 const graphql = require('graphql');
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID } = graphql;
+
+const movies = [
+  { id: '1', name: 'Pulp Fiction', genre: 'Crime' },
+  { id: '2', name: 'Alien', genre: 'Horror' },
+  { id: 3, name: 'Evangelion', genre: 'Anime' },
+  { id: 4, name: 'Marriage Story', genre: 'Drama' },
+];
 
 const MovieType = new GraphQLObjectType({
   name: 'Movie',
   fields: () => ({
-    id: { type: GraphQLString },
+    id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
   }),
@@ -16,9 +23,9 @@ const Query = new GraphQLObjectType({
   fields: { 
     movie: {
       type: MovieType,
-      args: { id: { type: GraphQLString } },
+      args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-  
+        return movies.find((movie) => movie.id == args.id);
       }
     },
   },
