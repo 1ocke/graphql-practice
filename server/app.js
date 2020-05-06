e7b88796dd0c652202797b8d6e44f489f089dd41
Andrew Zabelin <a.zabelin1@yandex.ru>
2020-05-07 00:20:10 +0300

    fix

--- server/app.js
+++ server/app.js
@@ -26,7 +26,7 @@ const dbConnection = mongoose.connection;
 dbConnection.on('error', (err) => console.log(`Connection error: ${err}`));
 dbConnection.once('open', () => console.log('Connected to DB'));
 
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, '..')));
 app.use(express.static(path.join(__dirname, '..', 'build')));
 
 app.get('/ping', function (req, res) {
