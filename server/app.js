6063af6c9372771865fe36611cec77e6fd07ca14
Andrew Zabelin <a.zabelin1@yandex.ru>
2020-05-06 21:29:59 +0300

    try to deploy

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ const env = require('dotenv');
 env.config();
 const app = express();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 const dbUrl = process.env.DB_CONN;
 
 mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true });
