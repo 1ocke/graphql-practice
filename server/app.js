1b36a8cebeab65ed4bb8eb864f942fc9beb39b22
Andrew Zabelin <a.zabelin1@yandex.ru>
2020-05-06 19:46:42 +0300

    try to deploy

---
+++ server/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const mongoose = require('mongoose');
+const schema = require('../schema/schema.js');
+const cors = require('cors');
+const env = require('dotenv');
+
+env.config();
+const app = express();
+
+const PORT = process.env.PORT;
+const dbUrl = process.env.DB_CONN;
+
+mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.use(cors());
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true,
+}));
+
+const dbConnection = mongoose.connection;
+dbConnection.on('error', (err) => console.log(`Connection error: ${err}`));
+dbConnection.once('open', () => console.log('Connected to DB'));
+
+app.listen(PORT || 5000, (err) => {
+  err ? console.log(err) : console.log('server started!');
+});
