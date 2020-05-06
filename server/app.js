031d0df0a9effbdcbeb45a96031b5d2dce91e7b2
Andrew Zabelin <a.zabelin1@yandex.ru>
2020-05-06 23:52:32 +0300

    try to deploy

--- server/app.js
+++ server/app.js
@@ -22,13 +22,13 @@ app.use('/graphql', graphqlHTTP({
   graphiql: true,
 }));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
-});
-
 const dbConnection = mongoose.connection;
 dbConnection.on('error', (err) => console.log(`Connection error: ${err}`));
 dbConnection.once('open', () => console.log('Connected to DB'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+});
 
 app.listen(PORT, HOST, (err) => {
   err ? console.log(err) : console.log('server started!');
