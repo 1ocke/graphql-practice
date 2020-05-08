e48ecf23263ab885eaf5334cdac4edb44323761e
Andrew Zabelin <a.zabelin1@yandex.ru>
2020-05-08 21:15:55 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { ApolloProvider } from 'react-apollo';
 import theme from './components/theme';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:3005/graphql',
+  uri: '/graphql',
 });
 
 class App extends Component {
